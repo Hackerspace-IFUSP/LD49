390cb5b40a888dca3df233d7e139bc0b2483fba8
Jaspior <ricardo.pitta@usp.br>
2021-10-04 13:42:11 -0300

    orbs

--- Scenes/Door.gd
+++ Scenes/Door.gd
@@ -6,5 +6,5 @@ export var orbs := 100
 
 
 func _on_Area2D_body_entered(body):
-	if body.name == "Player" and body.orbs == orbs:
+	if body.name == "Player" and body.orbs >= orbs:
 		$AnimationPlayer.play("open")
