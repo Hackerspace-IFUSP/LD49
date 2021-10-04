e0c6f1f6aa5fdce95233d6b6d6602cd95708f042
KeichiTS <kkeichi@gmail.co>
2021-10-04 19:08:23 -0300

    bug_fix

--- Scripts/thankyou.gd
+++ Scripts/thankyou.gd
@@ -2,8 +2,12 @@ extends Control
 
 func _ready():
 	$anim.play("event")
-	yield($anim,"animation_finished")
+	#yield($anim,"animation_finished")
 
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Scenes/Title_screen.tscn")
+
+
+func black_screen_del():
+	$black_screen.queue_free()
