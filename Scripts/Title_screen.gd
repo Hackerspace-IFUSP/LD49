0fec6db20af03b597251c3511011c44267536289
KeichiTS <kkeichi@gmail.co>
2021-10-04 18:03:14 -0300

    title_screen

---
+++ Scripts/Title_screen.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$anim.play("event")
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Maps/Test.tscn")
